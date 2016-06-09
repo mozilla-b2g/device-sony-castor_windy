889ee53516d98ddfc277a02a7e2c5b06a0f7d255
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2016-06-09 19:44:10 +0200

    Bug 1279286 - Welcome castor_windy device to the B2G family r=me

--- extract-files.sh
+++ extract-files.sh
@@ -1,4 +1,6 @@
-# Copyright 2014 The Android Open Source Project
+#!/bin/bash
+
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/castor_windy.mk
+DEVICE=castor_windy
+MANUFACTURER=sony
+STOCK=23.4.A.1.264
+
+. ../common/extract-files.sh
